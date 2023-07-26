0f60e9665fda99ded449f44a5a1700b08ab02eb8
Angela Lazzaro <angela.lazzaro0@gmail.com>
2023-07-26 18:11:23 -0400

    Imported and edited 3d model, made mobile compatability, started Intro

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { Tilt } from 'react-tilt'
+import { motion } from 'framer-motion'
+
+import { styles } from '../styles'
+import { services } from '../constants'
+import { fadeIn, textVariant } from '../utils/motion'
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <motion.div variants={textVariant()}>
+        <p classNames={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+      <motion.p
+        variants={fadeIn('','',0.1, 1)}
+      >
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, quaerat commodi corporis quod recusandae inventore repellendus sequi, fugiat assumenda soluta deleniti nemo esse ipsa minus labore eos dolorum officiis ut. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque deserunt magni illo maxime nisi temporibus quaerat adipisci sint debitis. Asperiores similique eum voluptatem expedita sequi temporibus doloremque! Reiciendis, excepturi soluta.
+
+      </motion.p>
+    </>
   )
 }
 
