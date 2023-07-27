30a8d60dba5bdefb141ac7f2c3d9802fa7e93843
Angela Lazzaro <angela.lazzaro0@gmail.com>
2023-07-27 15:44:46 -0400

    card animations

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,6 +6,33 @@ import { styles } from '../styles'
 import { services } from '../constants'
 import { fadeIn, textVariant } from '../utils/motion'
 
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <Tilt className='xs:w-[250px] w-full'>
+      <motion.div
+      variants={fadeIn('right', 'spring', 0.5*index, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+      >
+        <div
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450
+          }}
+          className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[250px] flex justify-evenly items-center flex-col'
+        >
+          <img src= {icon} alt={title} className='w-16 h-16 object-contain'/>
+          <h3 className='text-white text-[20px] font-bold text-center'>{title}</h3>
+
+      </div>
+
+      </motion.div>
+
+    </Tilt>
+
+  )
+}
+
 const About = () => {
   return (
     <>
@@ -19,6 +46,12 @@ const About = () => {
       >
         I'm a skilled software developer with experience in TypeScript and JavaScript, and expertise in frameworks like React, Node.js, and Three.js. I'm a quick learner and collaborate closely with clients to create efficient, scalable, and user-friendly solution that solve real-world problems. Let's work together to bring your ideas to life!
       </motion.p>
+
+      <div className='mt-30 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
     </>
   )
 }
